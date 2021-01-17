26317705f46af476ef5b9bb9daa3a4777b27c14c
vitius <vitek.manasek@seznam.cz>
2021-01-17 20:07:33 +0100

    0.3

--- memo/get_tree_array.php
+++ memo/get_tree_array.php
@@ -1,4 +1,33 @@
 <?php
+// edit leaf name of tree
+function edit_name($string) {
+  //PhyOp_Range TBL: dbo.oddeleni(alias TBL: o)(0) ASC  Bmk ( COL: Bmk1000 ) IsRow: COL: IsBaseRow1001
+  if(strpos($string, " TBL: ")) {
+    $pom=explode("TBL: ", $string);
+    $final=$pom[0]."<br>";
+    $pom2=explode("(",$pom[1]);
+    $final.=$pom2[0];
+    $pom3=explode("(alias TBL: ",$string);
+    $pom4=explode(" Bmk ",$pom3[1]);
+    $final.=" alias(".$pom4[0];
+  //PhyOp_StreamGbAgg( QCOL: [c].display_name) SORT-REQUEST( QCOL: [c].display_name)
+  } elseif(strpos($string, "(  QCOL: ")){
+    $pom=explode("(  QCOL: ", $string);
+    $final=$pom[0]."<br>";
+    $pom2=explode(") ",$pom[1]);
+    $final.="col: ".$pom2[0]."<br>";
+    $pom3=explode("SORT-REQUEST(  QCOL: ",$string);
+    $pom4=explode(")",$pom3[1]);
+    $final.="sort: ".$pom4[0];
+  } else {
+    $pom=explode(" ", trim($string));
+    $pom2=explode($pom[0], $string);
+    $string=str_replace($pom2[1]," ",$string);
+    $final=$string."<br>".$pom2[1];
+  }
+  return $final;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $final = $_POST['8607'];
   if (!empty($final)) {
@@ -9,19 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $string="";
     foreach ($arr as $value) {
       if(strlen(trim($value)) == 0 && $string!="") {
-
-        //PhyOp_Range TBL: dbo.oddeleni(alias TBL: o)(0) ASC  Bmk ( COL: Bmk1000 ) IsRow: COL: IsBaseRow1001
-        if(strpos($string, " TBL: ")) {
-          $pom=explode("TBL: ", $string);
-          $final=$pom[0];
-          $pom2=explode(" Bmk",$string);
-          $final.=$pom2[0];
-          array_push($tree,$final);
-        } else {
-          array_push($tree,$string);
-        }
+        array_push($tree,edit_name($string));
         $string="";
       } else {
+        //after empty row save leaf of tree
         $string.=$value;
       }
     }
--- memo/show_diagram.php
+++ memo/show_diagram.php
@@ -50,6 +50,7 @@ function find_cost($resultArray, $group, $subgroup) {
 }
 
 function find_minimum_cost($resultArray, $group) {
+  $subgroups=[];
   foreach ($resultArray[$group]["subgroup"] as $pom) {
     foreach ($pom as $key => $value) {
       if(isset($value["cost"])) {
@@ -58,6 +59,7 @@ function find_minimum_cost($resultArray, $group) {
         } else {
           $cost=$value["cost"];
         }
+        array_push($subgroups,$key);
         if(!isset($minimum)) {
           $minimum=$cost;
           $min_gr=$key;
@@ -68,12 +70,11 @@ function find_minimum_cost($resultArray, $group) {
       }
     }
   }
-  return $min_gr;
-}
-
-
+  return ["minimum"=>$min_gr,"all"=>$subgroups];
+}
+
+
 function find_tree($name, $tree, $key, $i) {
-
     $tree_name=explode(" ", $tree[$key+1]);
     if(isset($tree_name[$i])) {
       if ($tree_name[$i]!= "" && strpos($name,$tree_name[$i])!==false) {
@@ -81,9 +82,37 @@ function find_tree($name, $tree, $key, $i) {
         return array($key+1,$tree[$key+1]);
       }
     }
-
   return array($key,$name);
 }
+
+function create_all_diagrams($resultArray, $group2, $subgroup2, $pom2){
+  ?>
+    <div style="display: none;" id="value_<?php echo $group2.'-'.$subgroup2 ?>">
+  <?php
+  // buttons
+  foreach ($pom2["all"] as $sg) {
+    ?>
+      <button class="groups" id="button_for_<?php echo $group2."-".$sg ?>">
+    <?php echo find_name($resultArray, $group2, $sg);
+    ?>
+      </button>
+    <?php
+  }
+
+  // diagrams
+  foreach ($pom2["all"] as $sg) {
+    $cost=find_cost($resultArray, $group2, $sg);
+    $name=find_name($resultArray, $group2, $sg);
+    ?><ul class="tree <?php echo $group2." ".$group2."-".$sg; ?>" <?php if($sg!=$subgroup2){ echo 'style="display: none;"'; } ?> >
+    <li><span><?php echo $group2.".".$sg." Cost: ".$cost."</br>".$name; ?>
+    </span><ul> <?php
+    find_child($resultArray, $group2, $sg);
+    ?></ul></li></ul><?php
+  }
+  ?>
+  </div>
+  <?php
+}
 
 // memo array, memo group, memo subgroup, tree array, tree key, number of spaces
 function find_child($resultArray, $group, $subgroup) {
@@ -99,16 +128,23 @@ function find_child($resultArray, $group, $subgroup) {
           $style='';
         } else {
           $group2=$value;
-          $subgroup2=find_minimum_cost($resultArray, $value);
-          $style=' style="background-color: #ADD8E6"';
+          $pom2=find_minimum_cost($resultArray, $value);
+          $subgroup2=$pom2["minimum"];
+          $style=' id="for_'.$group2.'-'.$subgroup2.'" class="subgroups" style="background-color: #ADD8E6; cursor: pointer;"';
           $value=$group2.".".$subgroup2;
+
         }
 
         $name=find_name($resultArray, $group2, $subgroup2);
         $cost=find_cost($resultArray, $group2, $subgroup2);
 
         if(child_exists($resultArray, $group2, $subgroup2)) {
-            ?><li><span<?php echo $style; ?>><?php echo $value." Cost: ".$cost."</br>".$name; ?></span><ul><?php
+            ?><li><span<?php echo $style; ?>><?php echo $value." Cost: ".$cost."</br>".$name; ?>
+            </span>
+            <?php if($style!=""){
+              create_all_diagrams($resultArray, $group2, $subgroup2, $pom2);
+            } ?>
+            <ul><?php
             //find_child($resultArray, $group2, $subgroup2);
             find_child($resultArray, $group2, $subgroup2);
             ?></ul></li><?php
@@ -132,7 +168,7 @@ function add_tree_to_memo($resultArray, $group, $subgroup, $tree, $key, $i){
           $subgroup2=$pom[1];
         } else {
           $group2=$value;
-          $subgroup2=find_minimum_cost($resultArray, $value);
+          $subgroup2=find_minimum_cost($resultArray, $value)["minimum"];
         }
 
         $name=find_name($resultArray, $group2, $subgroup2);
@@ -143,20 +179,26 @@ function add_tree_to_memo($resultArray, $group, $subgroup, $tree, $key, $i){
         $resultArray=update_array($resultArray, $group2, $subgroup2, $name);
 
         if(child_exists($resultArray, $group2, $subgroup2)) {
-            //find_child($resultArray, $group2, $subgroup2);
-            $pom3=add_tree_to_memo($resultArray, $group2, $subgroup2, $tree, $key, $i+4);
-            $key=$pom3["key"];
-            $resultArray=$pom3["array"];
+          //find_child($resultArray, $group2, $subgroup2);
+          $pom3=add_tree_to_memo($resultArray, $group2, $subgroup2, $tree, $key, $i+4);
+          $key=$pom3["key"];
+          $resultArray=$pom3["array"];
         }
       }
     }
   }
   return ["key"=>$key,"array"=>$resultArray];
 }
-
-// this show diagram
+
+?>
+<div id="floor1">
+<?php
+// fill memo with tree and show button
 foreach ($resultArray[$root]["subgroup"] as $pom) {
   foreach ($pom as $key => $value) {
+    ?><button class="groups" id="button_for_<?php echo $root."-".$key ?>"><?php
+    echo $value["name"];
+    ?></button><?php
     $tree_name=explode(" ", $tree[0]);
     if(strpos($tree_name[8],$value["name"])!==false) {
       $resultArray=add_tree_to_memo($resultArray, $root, $key, $tree, 0, 12)["array"];
@@ -164,20 +206,83 @@ foreach ($resultArray[$root]["subgroup"] as $pom) {
   }
 }
 
+// show diagram
 foreach ($resultArray[$root]["subgroup"] as $pom) {
   foreach ($pom as $key => $value) {
     $tree_name=explode(" ", $tree[0]);
-    ?><ul class="tree"><?php
     // if in tree
     if(strpos($tree_name[8],$value["name"])!==false) {
-      ?><li><span><?php echo "Group: ".$root.".".$key."  Cost: ".$value["cost"]."</br>".$tree[0] ?></span><ul><?php
+      ?><ul class="tree <?php echo $root." ".$root."-".$key ?>"><li><span><?php echo "Group: ".$root.".".$key."  Cost: ".$value["cost"]."</br>".$tree[0] ?></span><ul><?php
       find_child($resultArray, $root, $key);
       ?></ul></li><?php
     } else {
-      ?><li><span><?php echo "Group: ".$root.".".$key."  Cost: ".$value["cost"]."</br>".$value["name"] ?></span><ul><?php
+      ?><ul class="tree <?php echo $root." ".$root."-".$key; ?>" style="display:none;"><li><span><?php echo "Group: ".$root.".".$key."  Cost: ".$value["cost"]."</br>".$value["name"] ?></span><ul><?php
       find_child($resultArray, $root, $key, ["tree"=>"", "key"=>0, "i"=>0]);
       ?></ul></li><?php
     }
     ?></ul><?php
   }
 }
+
+?>
+
+
+</div>
+2
+<div id="floor2">
+
+</div>
+3
+<div id="floor3">
+
+</div>
+4
+<div id="floor4">
+
+</div>
+5
+<div id="floor5">
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+
+
+$('body').on('click', ' .groups', function() {
+  var id = this.id.split("button_for_")[1];
+  $("."+id.split("-")[0]).hide();
+  $("."+id).show();
+});
+
+$('#floor1').on('click', '.subgroups',function(){
+  var id = "#value_"+this.id.split("for_")[1];
+  var content = $(id).html();
+  $('#floor2').html(content);
+  $('#floor3').html("");
+  $('#floor4').html("");
+  $('#floor5').html("");
+});
+
+$('#floor2').on('click', '.subgroups',function(){
+  var id = "#value_"+this.id.split("for_")[1];
+  var content = $(id).html();
+  $('#floor3').html(content);
+  $('#floor4').html("");
+  $('#floor5').html("");
+});
+
+$('#floor3').on('click', '.subgroups',function(){
+  var id = "#value_"+this.id.split("for_")[1];
+  var content = $(id).html();
+  $('#floor4').html(content);
+  $('#floor5').html("");
+});
+
+$('#floor4').on('click', '.subgroups',function(){
+  var id = "#value_"+this.id.split("for_")[1];
+  var content = $(id).html();
+  $('#floor5').html(content);
+});
+
+
+</script>
